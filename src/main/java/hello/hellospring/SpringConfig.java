8eb44f584ab94e6d004d2bf2155f2839a38eff2f
sedna99 <ljsmmm1228@gmail.com>
2024-04-13 17:19:03 +0900

    섹션7. AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,10 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceApp;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -25,9 +21,9 @@ public class SpringConfig {
 
 //    @Bean
 //    public MemberRepository memberRepository() {
-////        return new MemoryMemberRepository();
-////        return new JdbcMemberRepository(dataSource);
-////        return new JdbcTemplateMemberRepository(dataSource);
-////        return new JpaMemberRepository(em);
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
 //    }
 }
---
+++ src/main/java/hello/hellospring/aop/TimeTraceApp.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceApp {
+
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint jointPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + jointPoint.toString());
+        try {
+            return jointPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + jointPoint.toString() + " " + timeMs + "ms");
+        }
+
+    }
+}
